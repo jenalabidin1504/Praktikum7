b297bc8bab1314e99d11bb7ccbdfae1c3f7fa8a7
Jenal Abidin <jenalabidin894114@gmail.com>
2020-11-10 12:30:49 +0700

    Praktikum 5
    Praktikum 5

---
+++ praktikum5/src/main/java/com/jenal_18102270/praktikum5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jenal_18102270.praktikum5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
